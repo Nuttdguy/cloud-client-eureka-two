6a72a04a81b0c7ce335f47301220ee93adf259cc
nuttdguy <pygnasak@gmail.com>
2017-08-16 12:35:43 -0500

    eureka client two

---
+++ src/main/java/com/cloud/client/eureka/controller/EurekaClientTwo.java
@@ -0,0 +1,22 @@
+package com.cloud.client.eureka.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EurekaClientTwo {
+
+	
+	@Value("${words}")
+	private String words;
+	
+	@RequestMapping("/")
+	public String getWord() {
+		String[] wordArray = words.split(",");
+		int idx = (int)Math.round(Math.random() * wordArray.length );
+		return wordArray[idx];
+	}
+	
+	
+}
