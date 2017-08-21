8664a820f459857a14cb2bc71502023f7f8a8af3
nuttdguy <pygnasak@gmail.com>
2017-08-21 12:26:03 -0500

    change serviceUrl and application class name.

--- src/main/java/com/cloud/client/eureka/controller/EurekaTwoController.java
+++ src/main/java/com/cloud/client/eureka/controller/EurekaTwoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EurekaClientTwo {
+public class EurekaTwoController {
 
 	
 	@Value("${words}")
